4872cb79c65cf3b3f87f8c9876710feb8941458f
indylec <indy.leclercq@manchester.ac.uk>
2015-07-14 17:27:30 +0100

    A commit

---
+++ angdiff/casa/smooth_cubes.py
@@ -0,0 +1,17 @@
+#This is just a set of CASA commands to smooth a cube, the example here is S1.
+#Not meant to be run out of the box, needs tweaking for whatever you're going to do with it.
+
+#cube to import
+
+
+#define the smoothing beam
+
+galbeam={"major":"36arcmin","minor":"36arcmin","pa":"0deg"}
+
+importfits("s1cube","s1cube.image","whichhdu=0")
+
+imsmooth(imagename='s1cube.image',beam=galbeam,targetres=True,outfile='s1cubesmooth.image')
+
+exportfits('s1cubesmooth.image','s1cubesmooth.fits')
+
+#rinse and repeat 
